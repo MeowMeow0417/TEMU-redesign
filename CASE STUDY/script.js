e6c427bfad8e0900262720d0db80cdf943e9bd98
Meow <108517390+MeowMeow0417@users.noreply.github.com>
2024-09-29 22:23:39 +0800

    added some finishing touches

--- CASE STUDY/script.js
+++ CASE STUDY/script.js
@@ -5,3 +5,11 @@ hamburger.addEventListener('click', () => {
     console.log("Hamburger clicked!"); // Check if this logs
     navLinks.classList.toggle('active');
 });
+
+
+document.querySelectorAll('.dropdown-toggle').forEach(button => {
+    button.addEventListener('click', () => {
+        const content = button.nextElementSibling;
+        content.classList.toggle('active');
+    });
+});
